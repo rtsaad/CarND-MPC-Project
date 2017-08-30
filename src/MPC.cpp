02eed5c7cca91f0a5e6d8cc8623d398c7c33a268
Rodrigo Tacla <digaots@gmail.com>
2017-08-30 16:07:08 +0000

    error with trace line

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -9,7 +9,7 @@ using CppAD::AD;
 
 // TODO: Set the timestep length and duration
 size_t N = 10;
-double dt = 0.1;
+double dt = 0.100;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -54,20 +54,20 @@ class FG_eval {
     // Any additions to the cost should be added to `fg[0]`.
     fg[0] = 0;
 
-    // The part of the cost based on the reference state.
+    //Cost related to the reference state.
     for (int t = 0; t < N; t++) {
       fg[0] += 2000*CppAD::pow(vars[cte_start + t] - ref_cte, 2);
       fg[0] += 2000*CppAD::pow(vars[epsi_start + t] - ref_epsi, 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
-    // Minimize the use of actuators.
+    //Minimize the use of actuators.
     for (int t = 0; t < N - 1; t++) {
       fg[0] += 5*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += 5*CppAD::pow(vars[a_start + t], 2);
     }
 
-    // Minimize the value gap between sequential actuations.
+    //Minimize the value gap between sequential actuations.
     for (int t = 0; t < N - 2; t++) {
       fg[0] += 200*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += 10*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
--- src/main.cpp
+++ src/main.cpp
@@ -92,67 +92,67 @@ int main() {
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
 
-	  for(int i=0; i < ptsx.size(); i++){
+      	  for(int i=0; i < ptsx.size(); i++){
+      
+      	    //shift car reference angle to 90 degrees
+      	    double shift_x = ptsx[i] -px;
+      	    double shift_y = ptsx[i] -py;
+      
+      	    ptsx[i] = (shift_x * cos(0-psi) - shift_y*sin(0-psi));
+      	    ptsy[i] = (shift_x * sin(0-psi) + shift_y*cos(0-psi));
+      	    
+      	  }
+      
+      	  double* ptrx = &ptsx[0];
+      	  Eigen::Map<Eigen::VectorXd> ptsx_transform(ptrx, 6);
+      	  
+      	  double* ptry = &ptsy[0];
+      	  Eigen::Map<Eigen::VectorXd> ptsy_transform(ptry, 6);
+      
+      	  auto coeffs = polyfit(ptsx_transform, ptsy_transform, 3);
+      
+      	  //moved x and psi to zero
+      	  double cte = polyeval(coeffs, 0);
+      	  double epsi = -atan(coeffs[1]);
 
-	    //shift car reference angle to 90 degrees
-	    double shift_x = ptsx[i] -px;
-	    double shift_y = ptsx[i] -py;
-
-	    ptsx[i] = (shift_x * cos(0-psi) - shift_y*sin(0-psi));
-	    ptsy[i] = (shift_x * sin(0-psi) + shift_y*cos(0-psi));
-	    
-	  }
-
-	  double* ptrx = &ptsx[0];
-	  Eigen::Map<Eigen::VectorXd> ptsx_transform(ptrx, 6);
-	  
-	  double* ptry = &ptsy[0];
-	  Eigen::Map<Eigen::VectorXd> ptsy_transform(ptry, 6);
-
-	  auto coeffs = polyfit(ptsx_transform, ptsy_transform, 3);
-
-	  //moved x and psi to zero
-	  double cte = polyeval(coeffs, 0);
-	  double epsi = -atan(coeffs[1]);
-
-	   /*
-          * TODO: Calculate steering angle and throttle using MPC.
-          *
-          * Both are in between [-1, 1].
-          *
-          */
-         
+      	   /*
+                * TODO: Calculate steering angle and throttle using MPC.
+                *
+                * Both are in between [-1, 1].
+                *
+                */
+               
           double steer_value = j[1]["steering_angle"];
           double throttle_value = j[1]["throttle"];
-
-	  Eigen::VectorXd state(6);
-	  state << 0,0,0,v,cte,epsi;
-
-	  auto vars = mpc.Solve(state, coeffs);
-
-	  //print polynomial back to simulator
-	  vector<double> next_x_vals;
-	  vector<double> next_y_vals;  
-	  //x value distance
-	  const double poly_inc = 2.5;
-	  //number of points to print
-	  const int num_points = 25;
-	  for(int i=1; i<num_points; i++){
-	    next_x_vals.push_back(poly_inc*1);
-	    next_y_vals.push_back(polyeval(coeffs, poly_inc*i));
-	  }
-
-	  vector<double> mpc_x_vals;
-	  vector<double> mpc_y_vals;
-	  for(int i=2; i<vars.size(); i++){
-	    if(i%2==0){
-	      mpc_x_vals.push_back(vars[i]);
-	    } else {
-	      mpc_y_vals.push_back(vars[i]);
-	    }
-	  }
-
-	  const double Lf = 2.67;
+      
+      	  Eigen::VectorXd state(6);
+      	  state << 0,0,0,v,cte,epsi;
+      
+      	  auto vars = mpc.Solve(state, coeffs);
+      
+      	  //print polynomial back to simulator
+      	  vector<double> next_x_vals;
+      	  vector<double> next_y_vals;  
+      	  //x value distance
+      	  const double poly_inc = 2.5;
+      	  //number of points to print
+      	  const int num_points = 25;
+      	  for(int i=1; i<num_points; i++){
+      	    next_x_vals.push_back(poly_inc*i);
+      	    next_y_vals.push_back(polyeval(coeffs, poly_inc*i));
+      	  }
+      
+      	  vector<double> mpc_x_vals;
+      	  vector<double> mpc_y_vals;
+      	  for(int i=2; i<vars.size(); i++){
+      	    if(i%2==0){
+      	      mpc_x_vals.push_back(vars[i]);
+      	    } else {
+      	      mpc_y_vals.push_back(vars[i]);
+      	    }
+      	  }
+      
+      	  const double Lf = 2.67;
 	  
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
@@ -160,7 +160,7 @@ int main() {
           msgJson["steering_angle"] = vars[0]/(deg2rad(25)*Lf);
           msgJson["throttle"] = vars[1];
 
-	  //Display the MPC predicted trajectory           
+	        //Display the MPC predicted trajectory           
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
 
